2b4c9c26ce7fdf39f58c3434612e5c61b4695ebf
zhanyd <zhanyd@sina.com>
2022-01-13 16:08:54 +0800

    add

--- The C Programming Language parcitce/chapter2/htoi.c
+++ The C Programming Language parcitce/chapter2/htoi.c
@@ -9,7 +9,7 @@
 #include <math.h>
 #include <string.h>
 
-#define MAXLINE 1000
+#define MAXLINE 100
 
 void getline(char s[]);
 
@@ -19,18 +19,22 @@ int main() {
 
 	getline(s);
 
-	printf("s = %s ", s);
+	printf("s = %s \n", s);
 
 	int sum,i;
 	int number = 0;
 	for(i = 0; i < strlen(s); i++) {
+		if(i == 0 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X')) {
+			i = 1;
+			continue;
+		}
 		if(s[i] >= '0' && s[i] <= '9') {
 			number = s[i] - '0';
 		} else {
 			number = lower(s[i]);
-			printf("number = %s ", number);
+			printf("number = %d \n", number);
 			number = 10 + s[i] - 'a';
-			printf("number = %d ", number);
+			printf("number = %d \n", number);
 		}
 		sum = sum + number * pow(16, strlen(s) - i - 1);
 	}
@@ -38,7 +42,7 @@ int main() {
 	printf("sum = %d ", sum);
 }
 
-
+// 大写字母转小写字母
 int lower(int c) {
 	if(c >= 'A' && c <= 'Z') {
 		return c + 'a' - 'A';
@@ -48,7 +52,7 @@ int lower(int c) {
 	}
 }
 
-
+// 从键盘输入字符串，回车结束
 void getline(char s[]) {
 	int c, i;
 
